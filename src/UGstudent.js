30b36557a7a78a1ef73f4f794d46a252a51b31fc
Eugene <yuchunlin0075@gmail.com>
2020-10-26 18:54:21 +0800

    update photos

--- src/UGstudent.js
+++ src/UGstudent.js
@@ -6,6 +6,7 @@ import Table from 'react-bootstrap/Table'
 import { Divider } from 'semantic-ui-react'
 import './MSstudents.css'
 import egimg from './imgs/DSC00098.png'
+import cyiimg from './imgs/741474.png'
 import Student from './student'
 
 class UGstudents extends React.Component{
@@ -33,8 +34,11 @@ class UGstudents extends React.Component{
                 </Row>
                 <Row>
                     <Col>
-                        <Student img={egimg} name={"林友鈞"} engName={"Eugene"}/>
+                        <Student img={egimg} name={"林友鈞"} engName={""}/>
                     </Col>
+					<Col>
+						<Student img={cyiimg} name={"許湘苡"} engName={""}/>
+					</Col>
                     {/* <Col>
                         <Student img={testAvatar} name={"林友鈞"} engName={"Eugene"}/>
                     </Col>
