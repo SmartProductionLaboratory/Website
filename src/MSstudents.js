c4091e9a907c39e4dad6e7db92b251c7a46dda88
effylee1006 <97221948+effylee1006@users.noreply.github.com>
2022-03-19 16:08:01 +0800

    add photos

--- src/MSstudents.js
+++ src/MSstudents.js
@@ -6,6 +6,12 @@ import Table from 'react-bootstrap/Table'
 import { Divider } from 'semantic-ui-react'
 import './styles/MSstudents.css'
 import jrimg from './imgs/zrl.png'
+import effyimg from './imgs/effy.png'
+import wadeimg from './imgs/wade.png'
+import phoneimg from './imgs/phone.png'
+import lianimg from './imgs/lian.png'
+import annieimg from './imgs/annie.png'
+import tiffanyimg from './imgs/tiffany.png'
 import gcimg from './imgs/jc.png'
 import pgimg from './imgs/pg.png'
 import nckuimg from './imgs/NCKU.png'
@@ -19,13 +25,14 @@ class MSstudents extends React.Component{
                 {img : jrimg, name : "梁哲榕", engName: ""},
                 {img : pgimg, name : "李沛倚", engName: ""},
                 {img : gcimg, name : "游智翔", engName: ""},
-                {img : nckuimg, name : "黃偉哲", engName: ""},
+                {img : wadeimg, name : "黃偉哲", engName: ""},
+
+                {img : annieimg, name : "王雅函", engName: ""},
+		{img : effyimg, name : "李雅婷", engName: ""},
+                {img : tiffanyimg, name : "唐嘉謙", engName: ""},
+                {img : lianimg, name : "何立安", engName: ""},
+                {img : phoneimg, name : "陳藝丰", engName: ""},
                 
-                {img : nckuimg, name : "唐嘉謙", engName: ""},
-                {img : nckuimg, name : "何立安", engName: ""},
-                {img : nckuimg, name : "陳藝丰", engName: ""},
-                {img : nckuimg, name : "李雅婷", engName: ""},
-                {img : nckuimg, name : "王雅函", engName: ""},
             ];
 
         
